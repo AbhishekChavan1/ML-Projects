9416d53c59e7e20c342827205673cfdc9cb360f7
Abhishek Mangesh Chavan <128388772+AbhishekChavan1@users.noreply.github.com>
2025-10-07 23:12:40 +0530

    Add files via upload

---
+++ Diabetes Prediction/app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("Diabetes_Model1.pkl")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict(): 
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    if (prediction[0] > 0.5):return render_template('index.html', prediction_text='RESULTS: You are in high risk of diabetes')    
+    else:return render_template('index.html', prediction_text= 'RESULTS: You are in low risk of diabetes')    
+
+if __name__ == "__main__":
+    app.run(debug=True)
